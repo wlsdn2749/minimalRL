69ea4d8c8c4e1f38172282c5432a7cd34f3e4e55
wlsdn2749 <wlsdn2749@gmail.com>
2023-12-23 19:17:43 +0900

    modified: dqn.py

--- dqn.py
+++ dqn.py
@@ -1,11 +1,15 @@
-import gym
+import gymnasium as gym
 import collections
 import random
+import glob 
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+import os
+import wandb 
 
 #Hyperparameters
 learning_rate = 0.0005
@@ -16,7 +20,10 @@ batch_size    = 32
 class ReplayBuffer():
     def __init__(self):
         self.buffer = collections.deque(maxlen=buffer_limit)
-    
+        
+    def __len__(self):
+        return len(self.buffer)
+     
     def put(self, transition):
         self.buffer.append(transition)
     
@@ -57,7 +64,7 @@ class Qnet(nn.Module):
         coin = random.random()
         if coin < epsilon:
             return random.randint(0,1)
-        else : 
+        else: 
             return out.argmax().item()
             
 def train(q, q_target, memory, optimizer):
@@ -75,7 +82,13 @@ def train(q, q_target, memory, optimizer):
         optimizer.step()
 
 def main():
-    env = gym.make('CartPole-v1')
+    run = wandb.init(
+        project = f'minRL_lightning_{os.path.basename(__file__)}'
+    )
+    env = gym.make('CartPole-v1', render_mode='rgb_array')
+    # env = gym.wrappers.RecordVideo(env, 
+    #                                video_folder=f'./videos/{run.id}', 
+    #                                episode_trigger= lambda x : x % 2000 == 0)
     q = Qnet()
     q_target = Qnet()
     q_target.load_state_dict(q.state_dict())
@@ -83,32 +96,49 @@ def main():
 
     print_interval = 20
     score = 0.0  
+    steps = 0
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
     for n_epi in range(10000):
         epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
         s, _ = env.reset()
         done = False
-
+        epi_per_step = 0
         while not done:
             a = q.sample_action(torch.from_numpy(s).float(), epsilon)      
             s_prime, r, done, truncated, info = env.step(a)
+            # print(s_prime, r, done, truncated, info)
+            steps += 1
+            epi_per_step += 1
+            print(epi_per_step, r, done, truncated)
             done_mask = 0.0 if done else 1.0
             memory.put((s,a,r/100.0,s_prime, done_mask))
             s = s_prime
 
             score += r
-            if done:
-                break
+            env.render()
             
-        if memory.size()>2000:
+        print(n_epi)
+            
+        if memory.size() > 2000:
             train(q, q_target, memory, optimizer)
 
-        if n_epi%print_interval==0 and n_epi!=0:
+        if n_epi % print_interval==0 and n_epi!=0:
             q_target.load_state_dict(q.state_dict())
             print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
                                                             n_epi, score/print_interval, memory.size(), epsilon*100))
             score = 0.0
+                      
+        wandb.log({"Reward": score,
+                   "Steps": steps,
+                   "Episode": n_epi,
+                   "Buffer size": memory.__len__()})
+            
+        mp4list = glob.glob(f'./videos/{run.id}/*.mp4')
+        if len(mp4list) > 1:
+            mp4 = mp4list[-2]            
+            wandb.log({"gameplays": wandb.Video(mp4, caption='episode: '+str(n_epi-2000), fps=4, format="gif"), "Episode": n_epi})
+        
     env.close()
 
 if __name__ == '__main__':
